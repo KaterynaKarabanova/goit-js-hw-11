5edd1d7a78cde576c66d9d5820c3dea85b05439d
Kateryna <ikatya7575@gmaiil.com>
2023-08-23 20:29:46 +0300

    add

--- src/common.js
+++ src/common.js
@@ -8,7 +8,7 @@ import "simplelightbox/dist/simple-lightbox.min.css";
     .map(({ webformatURL, largeImageURL, tags, likes, views,comments, downloads }) => `
       <div class="photo-card">
         <a class="gallery__link" href="${largeImageURL}">
-          <img src="${webformatURL}" alt="${tags}" class="gallery-image" loading="lazy" width="300" height="200">
+          <img src="${webformatURL}" alt="${tags}" class="gallery-image" loading="lazy" >
           <div class="info">
             <p class="info-item">
                 <b>Likes </b><br>
